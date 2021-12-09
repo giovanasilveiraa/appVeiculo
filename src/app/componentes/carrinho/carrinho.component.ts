288d86c8aef3479521f08e495843e1af5c443e1d
giovanasilveiraa <giovana.silveira01@etec.sp.gov.br>
2021-12-09 14:30:03 -0300

    ok

--- src/app/componentes/carrinho/carrinho.component.ts
+++ src/app/componentes/carrinho/carrinho.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2/dist/sweetalert2.js';
 
 @Component({
   selector: 'app-carrinho',
@@ -20,6 +21,31 @@ export class CarrinhoComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  confirmBox(){
+    Swal.fire({
+      title: 'Are you sure want to remove?',
+      text: 'You will not be able to recover this file!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, delete it!',
+      cancelButtonText: 'No, keep it'
+    }).then((result) => {
+      if (result.value) {
+        Swal.fire(
+          'Deleted!',
+          'Your imaginary file has been deleted.',
+          'success'
+        )
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        Swal.fire(
+          'Cancelled',
+          'Your imaginary file is safe :)',
+          'error'
+        )
+      }
+    })
   }
 
 }
